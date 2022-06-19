a923689065bf35c23b63110b3661524fa8671d6b
Andraš <andras.tsitskan@gmail.com>
2022-06-19 14:07:56 +0300

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (initialArray, ...removables) {
+  let newArray = initialArray;
 
+  for (const removable of removables) {
+    // console.log(removable);
+    newArray = newArray.filter((item) => {
+      return item !== removable;
+    });
+    // console.log(newArray);
+  }
+
+  //   console.log(newArray);
+  //   console.log(initialArray);
+  return newArray;
 };
 
 // Do not edit below this line
