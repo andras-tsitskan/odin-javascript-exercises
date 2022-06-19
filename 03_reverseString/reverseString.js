08cbacdd4c1b410e1172e856bfa1bd1a292e218f
Andraš <andras.tsitskan@gmail.com>
2022-06-19 10:32:10 +0300

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+// const reverseString = function (string) {
+//   const lettersArray = [];
 
+//   for (const letter of string) {
+//     lettersArray.push(letter);
+//   }
+
+//   const reversedArray = [];
+
+//   for (let i = lettersArray.length - 1; i >= 0; i--) {
+//     reversedArray.push(lettersArray[i]);
+//   }
+
+//   return reversedArray.join("");
+// };
+
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
 };
+
+// reverseString("Hello world");
 
 // Do not edit below this line
 module.exports = reverseString;
