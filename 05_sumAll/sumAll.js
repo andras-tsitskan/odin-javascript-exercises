67d1d4bfcee6179113eb08aa7c02e4b31a0ee0e1
Andraš <andras.tsitskan@gmail.com>
2022-06-19 14:37:57 +0300

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let finalSum = 0;
 
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
+
+  if (num1 > num2) {
+    while (num1 >= num2) {
+      finalSum += num1;
+      num1--;
+    }
+  } else {
+    while (num1 <= num2) {
+      finalSum += num1;
+      num1++;
+    }
+  }
+
+  return finalSum;
 };
 
 // Do not edit below this line
