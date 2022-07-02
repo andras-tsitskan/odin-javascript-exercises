c9b0576852393b5039611ba80a060aa56a2da72b
Andraš <andras.tsitskan@gmail.com>
2022-07-02 21:28:30 +0300

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  } else if (n === 0) {
+    return 0;
+  } else {
+    const array = [1, 1];
 
+    for (let i = 2; i <= n; i++) {
+      array.push(array.at(-1) + array.at(-2));
+    }
+
+    return array[n - 1];
+  }
 };
+
+//   ODIN SOLUTION:
+
+// const fibonacci = function(count) {
+//     if (count < 0) return "OOPS";
+//     if (count === 0) return 0;
+//     let a = 0;
+//     let b = 1;
+//     for (let i = 1; i < count; i++) {
+//       const temp = b;
+//       b = a + b;
+//       a = temp;
+//     }
+//     return b;
+//   };
 
 // Do not edit below this line
 module.exports = fibonacci;
