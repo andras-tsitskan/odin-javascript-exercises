7714107bbe23960c33c96d88537fc8f0276cfc0d
Andraš <andras.tsitskan@gmail.com>
2022-07-02 11:21:43 +0300

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const regex = /\W/gi;
+  string = string.toLowerCase().replaceAll(regex, "");
 
+  let reversed = string.split("").reverse().join("");
+
+  console.log(string);
+  console.log(reversed);
+
+  return string === reversed;
 };
 
 // Do not edit below this line
