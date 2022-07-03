f9ee00c3252ba38a63602765efae18d1f6facfb9
Andraš <andras.tsitskan@gmail.com>
2022-07-03 15:54:41 +0300

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,55 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  array.sort((a, b) => {
+    if (a.yearOfDeath && b.yearOfDeath) {
+      const previous = a.yearOfBirth - a.yearOfDeath;
+      const next = b.yearOfBirth - b.yearOfDeath;
 
+      if (previous < next) {
+        return -1;
+      } else if (previous === next) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else {
+      a.yearOfDeath = new Date().getFullYear();
+      b.yearOfDeath = new Date().getFullYear();
+
+      const previous = a.yearOfBirth - a.yearOfDeath;
+      const next = b.yearOfBirth - b.yearOfDeath;
+
+      if (previous < next) {
+        return -1;
+      } else if (previous === next) {
+        return 0;
+      } else {
+        return 1;
+      }
+    }
+  });
+
+  return array[0];
 };
+
+// ODIN SOLUTION
+
+// const findTheOldest = function (array) {
+//   return array.reduce((oldest, currentPerson) => {
+//     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+//     const currentAge = getAge(
+//       currentPerson.yearOfBirth,
+//       currentPerson.yearOfDeath
+//     );
+//     return oldestAge < currentAge ? currentPerson : oldest;
+//   });
+// };
+
+// const getAge = function (birth, death) {
+//   if (!death) {
+//     death = new Date().getFullYear();
+//   }
+//   return death - birth;
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
